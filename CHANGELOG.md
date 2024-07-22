7a1f09a1523ba94fe94ea6da6c998d2a3a89573b
Elton Santos <eltonaxl@hotmail.com>
2024-07-22 08:04:29 -0300

    Add json, add method and change readme

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 0.1.1
+
+- Add json with all banks (updated until Jul 13, 2024)
+- Add methods
+- Change README
+
 ## 0.1.0
 
 - First release
--- lib/bancos_brasileiros.rb
+++ lib/bancos_brasileiros.rb
@@ -1,8 +1,37 @@
 # frozen_string_literal: true
 
+require 'json'
 require_relative "bancos_brasileiros/version"
 
 module BancosBrasileiros
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    def load_data
+      file_path = File.expand_path('../../data/bancos.json', __FILE__)
+      JSON.parse(File.read(file_path))
+    end
+
+    def all_banks
+      load_data
+    end
+
+    def find_bank_by_codigo_compe(compe)
+      all_banks.find { |bank| bank["COMPE"] == compe }
+    end
+
+    def find_bank_by_ispb(ispb)
+      all_banks.find { |bank| bank["ISPB"] == ispb }
+    end
+
+    def find_banks_by_network(network)
+      all_banks.select { |bank| bank["Network"] == network }
+    end
+
+    def find_banks_by_type(bank_type)
+      all_banks.select { |bank| bank["Type"] == bank_type }
+    end
+
+    def find_banks_by_pix_type(pix_type)
+      all_banks.select { |bank| bank["PixType"] == pix_type }
+    end
+  end
 end
